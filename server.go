6b3bb4b34155798fe27b391cdca776b627d93bb3
nodarby <ndtmhr@outlook.jp>
2019-11-15 00:47:54 +0900

    youtubeと連携

--- server.go
+++ server.go
@@ -1,12 +1,29 @@
 package main
 
 import (
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	"github.com/sirupsen/logrus"
 	"youtubeApp-by-go/routes"
 )
 
+func init(){
+	err := godotenv.Load()
+	if err != nil{
+		logrus.Fatal("Error loading ./env")
+	}
+
+	logrus.SetLevel(logrus.DebugLevel)
+	logrus.SetFormatter(&logrus.JSONFormatter{})
+}
+
 func main()  {
 	e := echo.New()
+
+	//Middlewares
+	e.Use(middleware.Logger())
+	e.Use(middleware.CORS())
 
 	//Routes
 	routes.Init(e)
--- web/api/fetch_most_popular_videos.go
+++ web/api/fetch_most_popular_videos.go
@@ -1,12 +1,35 @@
 package api
 
 import (
+	"context"
 	"github.com/labstack/echo"
+	"github.com/sirupsen/logrus"
 	"github.com/valyala/fasthttp"
+	"google.golang.org/api/option"
+	"google.golang.org/api/youtube/v3"
+	"os"
 )
 
 func FetchMostPopularVideos() echo.HandlerFunc{
 	return func(c echo.Context) error {
-			return c.JSON(fasthttp.StatusOK, "Most Popular")
+			key := os.Getenv("API_KEY")
+
+			ctx := context.Background()
+			yts, err := youtube.NewService(ctx, option.WithAPIKey(key))
+			if err != nil{
+				logrus.Fatalf("Error creating new Youtube service: %v",err)
+			}
+
+			call := yts.Videos.
+				    List("id,snippet").
+					Chart("mostPopular").
+					MaxResults(3)
+
+			res, err := call.Do()
+			if err != nil{
+				logrus.Fatalf("Error calling Youtube API: %v", err)
+			}
+
+			return c.JSON(fasthttp.StatusOK, res)
 	}
 }
